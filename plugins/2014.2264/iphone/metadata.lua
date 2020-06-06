6c0c1179797c21a9e9138163ae75b823b04edd13
Vlad Shcherban <vlad@coronalabs.com>
2020-06-06 19:54:26 +0300

    Updating binaries

---
+++ plugins/2014.2264/iphone/metadata.lua
@@ -0,0 +1,17 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+
+		-- This is the name without the 'lib' prefix.
+		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
+		staticLibs = { 'plugin_airprint' }, 
+
+		frameworks = { },
+		frameworksOptional = {},
+		delegates = {  }
+	},
+}
+
+return metadata
